24004a7565259f9cf71ff945b65c6e4a999e1f3d
Samuel Cornejo <sam.ach1990@gmail.com>
2016-11-01 14:31:31 -0600

    Finished creating the layout of DetailView and coding so the DetailView displays the correct data for the selected event

--- PhunApp/Views/DetailView/PHADetailViewController.m
+++ PhunApp/Views/DetailView/PHADetailViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "PHADetailViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface PHADetailViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UINavigationItem *nagivationItem;
+@property (weak, nonatomic) IBOutlet UIImageView *gradientImageView;
+@property (weak, nonatomic) IBOutlet UILabel *dateTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleTextLabel;
+@property (weak, nonatomic) IBOutlet UITextView *eventDescriptionTextView;
 
 @end
 
@@ -16,12 +23,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    self.automaticallyAdjustsScrollViewInsets = false;
+    
+    [self applyGradientToImageView];
+    [self makeNavigationBarTransparent];
+    [self fillUpTextFields];
 }
 
 /*
@@ -33,5 +40,49 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - UI
+
+- (void)makeNavigationBarTransparent {
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+                                                  forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.view.backgroundColor = [UIColor clearColor];
+    self.navigationController.navigationBar.backgroundColor = [UIColor clearColor];
+    self.navigationController.navigationBar.tintColor = [UIColor blackColor];
+}
+
+- (void)applyGradientToImageView {
+    [self.imageView sd_setImageWithURL:[NSURL URLWithString:self.eventToDisplay.imageURL]];
+    
+    CAGradientLayer *layer = [CAGradientLayer layer];
+    layer.frame = self.imageView.frame;
+    layer.colors = @[(__bridge id)[UIColor clearColor].CGColor,   // start color
+                     (__bridge id)[UIColor whiteColor].CGColor]; // end color
+    
+    UIGraphicsBeginImageContext(self.imageView.frame.size);
+    [layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    self.gradientImageView.image = image;
+}
+
+- (void)fillUpTextFields {
+    self.dateTextLabel.text = [[self dateFormatter] stringFromDate:self.eventToDisplay.eventDate];
+    self.titleTextLabel.text = self.eventToDisplay.eventTitle;
+    self.eventDescriptionTextView.text = self.eventToDisplay.eventDescription;
+}
+
+#pragma mark - Helper Methods
+
+- (NSDateFormatter *)dateFormatter {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"MMMM dd, yyyy 'at' hh:mm a"];;
+    [dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];
+    
+    return dateFormatter;
+}
 
 @end
