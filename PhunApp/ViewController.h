8b79fc032398abdc96761c219faab97e88ec58bb
Samuel Cornejo <sam.ach1990@gmail.com>
2016-10-31 21:40:31 -0600

    Added .gitignore, Podfile and 3rd party libraries

---
+++ PhunApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhunApp
+//
+//  Created by Samuel Cornejo on 10/31/16.
+//  Copyright © 2016 Applaudo Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhunApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhunApp
+//
+//  Created by Samuel Cornejo on 10/31/16.
+//  Copyright © 2016 Applaudo Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
